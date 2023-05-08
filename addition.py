3842187a93e83fb73967a9a7bceb9dcc286027f5
sujeeshpt <132337992+sujeeshpt@users.noreply.github.com>
2023-05-08 15:59:15 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,15 @@
+a=int(input("enter the number "))
+b=int(input("enter the second  number "))
+choice=int(input("choice:"))
+if choice==1:
+    c=a+b
+    print("addition",c)
+elif choice==2:
+    c=a-b
+    print("subtration",c)
+elif choice==3:
+    c=a*b
+    print("multiplication",c)
+elif choice==4:
+    c=a/b
+    print("division",c)
